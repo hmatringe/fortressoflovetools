cccc345d7eec6148c8017b6a6f05bff963ec30ec
Matringe <hadrien.matringe@gmail.com>
2017-12-15 21:39:17 +0100

    commenting TestJob to test Travis on PR

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,8 +2,8 @@ class TestJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    puts "I'm starting with user with the id #{user_id}"
-    sleep 2
+    # puts "I'm starting with user with the id #{user_id}"
+    # sleep 2
     puts "done"
   end
 end
