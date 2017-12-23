bdfcba8a76683ead421c2f1291ce12dcb1deb7e8
Matringe <hadrien.matringe@gmail.com>
2017-12-23 12:03:04 +0100

    fix attempt post controller test

--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
-  test "should get receive_fetched_sales_orders" do
-    get webhooks_receive_fetched_sales_orders_url
-    assert_response :success
+  test "should post receive_fetched_sales_orders" do
+    post webhooks_receive_fetched_sales_orders_url
+    assert_response :nothing
   end
 
 end
