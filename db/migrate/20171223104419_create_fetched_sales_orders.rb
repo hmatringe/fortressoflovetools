c7b0e3ea757e2c17d1dd8521072aa374145aee4b
Matringe <hadrien.matringe@gmail.com>
2017-12-23 12:29:26 +0100

    restored migration festched sales order

---
+++ db/migrate/20171223104419_create_fetched_sales_orders.rb
@@ -0,0 +1,10 @@
+class CreateFetchedSalesOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :fetched_sales_orders do |t|
+      t.string :woocommerce_sales_order_id
+      t.json :body
+
+      t.timestamps
+    end
+  end
+end
